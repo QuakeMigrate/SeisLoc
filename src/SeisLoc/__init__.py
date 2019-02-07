edd348a2dabd5d9a7c2848938b9dc71c0461bc82
Jonathan Smith <jds70@cam.ac.uk>
2019-02-07 15:38:42 +0000

    Upadting __init__.py

--- src/SeisLoc/__init__.py
+++ src/SeisLoc/__init__.py
@@ -1,7 +1,7 @@
 name = "SeisLoc"
 __version__ = "0.2"
-__description__ = "Coalescence Microseismic Scanning - CMScanning"
+__description__ = "SeisLoc - Coalescence Microseismic Location Analysis"
 __license__ = "GPL v3.0"
-__author__ = "Jonathan Smith, Julian Drew, Tom Hudson, and Tom Winder"
-__email__ = "jds70@cam.ac.uk"
+__author__ = "Jonathan Smith, Tom Hudson, Tom Winder and Julian Drew,"
+__email__ = "jds70@cantab.net"
 
