0861ec1fc2b4ba1209d10eb3f0f9ed5554080f9a
Ulvetanna <jds70@cam.ac.uk>
2019-03-27 12:45:44 +0000

    Removing Install packages

--- build/lib/SeisLoc/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.2"
-__description__ = "Coalescence Microseismic Scanning - CMScanning"
-__license__ = "GPL v3.0"
-__author__ = "Jonathan Smith, Julian Drew, Tom Hudson, and Tom Winder"
-__email__ = "jds70@cam.ac.uk"
-
